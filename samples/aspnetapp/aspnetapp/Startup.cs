3c943cff73d670b86f09b851a56bd1c22c7f6989
Pakkianthan, Pratheep <pratheep_pp@yahoo.com>
2019-10-29 17:05:26 +0100

    Added prometheus, swagger and serilog

--- samples/aspnetapp/aspnetapp/Startup.cs
+++ samples/aspnetapp/aspnetapp/Startup.cs
@@ -9,6 +9,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Prometheus;
+
+using Microsoft.OpenApi.Models;
+
+//Adding swagger: https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-3.0&tabs=visual-studio
+//Adding serilog: https://github.com/serilog/serilog-aspnetcore
+//Adding Prometheus: https://www.c-sharpcorner.com/article/reporting-metrics-to-prometheus-in-asp-net-core/
+
 namespace aspnetapp
 {
     public class Startup
@@ -23,7 +31,11 @@ namespace aspnetapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,25 +47,22 @@ namespace aspnetapp
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
+            app.UseMetricServer();
             app.UseRouting();
 
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                /*
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                    */
             });
         }
     }
